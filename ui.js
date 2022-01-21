de8ba78047222e3ed121e4471ebff81ebf3b9f18
mari <63924379+Cookieglue@users.noreply.github.com>
2022-01-20 20:59:30 -0500

    Fixes vertical scaling issue on graph

--- ui.js
+++ ui.js
@@ -24,36 +24,36 @@ function performanceGraph (top, med, bot){
 	strokeWeight(2);
 	
 	//x axis
-	line(pad*4,windowHeight/2 -pad*4,width/2 -pad*4 ,windowHeight/2 -pad*4);
+	line(pad*4,height/2 -pad*4,width/2 -pad*4 ,height/2 -pad*4);
 	//y axis
-	line(pad*4,pad*4,pad*4 ,windowHeight/2 -pad*4);
+	line(pad*4,pad*4,pad*4 ,height/2 -pad*4);
 	
 	//top
 	stroke(0,255,0);
 	for (var i = 0; i < top.length ; i++){
 		strokeWeight(8);
-		point(length*i + pad*5, (windowHeight/2 - pad * 4 ) - graphHeight*top[i]);
+		point(length*i + pad*5, (height/2 - pad * 4 ) - graphHeight*top[i]);
 		strokeWeight(1);
-		line(length*i + pad*5, (windowHeight/2 - pad * 4 ) - graphHeight*top[i],
-				length*(i-1) + pad*5, (windowHeight/2 - pad * 4 ) - graphHeight*top[i-1]);
+		line(length*i + pad*5, (height/2 - pad * 4 ) - graphHeight*top[i],
+				length*(i-1) + pad*5, (height/2 - pad * 4 ) - graphHeight*top[i-1]);
 	}
 	//med
 	stroke(255);
 	for (var i = 0; i < med.length ; i++){
 		strokeWeight(8);
-		point(length*i + pad*5, (windowHeight/2 - pad * 4 ) - graphHeight*med[i]);
+		point(length*i + pad*5, (height/2 - pad * 4 ) - graphHeight*med[i]);
 		strokeWeight(1);
-		line(length*i + pad*5, (windowHeight/2 - pad * 4 ) - graphHeight*med[i],
-				length*(i-1) + pad*5, (windowHeight/2 - pad * 4 ) - graphHeight*med[i-1]);
+		line(length*i + pad*5, (height/2 - pad * 4 ) - graphHeight*med[i],
+				length*(i-1) + pad*5, (height/2 - pad * 4 ) - graphHeight*med[i-1]);
 	}
 	//bot
 	stroke(255,0,0);
 	for (var i = 0; i < bot.length ; i++){
 		strokeWeight(8);
-		point(length*i + pad*5, (windowHeight/2 - pad * 4 ) - graphHeight*bot[i]);
+		point(length*i + pad*5, (height/2 - pad * 4 ) - graphHeight*bot[i]);
 		strokeWeight(1);
-		line(length*i + pad*5, (windowHeight/2 - pad * 4 ) - graphHeight*bot[i],
-				length*(i-1) + pad*5, (windowHeight/2 - pad * 4 ) - graphHeight*bot[i-1]);
+		line(length*i + pad*5, (height/2 - pad * 4 ) - graphHeight*bot[i],
+				length*(i-1) + pad*5, (height/2 - pad * 4 ) - graphHeight*bot[i-1]);
 	}
 	noStroke();
 }
